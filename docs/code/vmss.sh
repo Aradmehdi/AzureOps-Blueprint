4ddcc576a9dc843c1cb27fbd6e4e1166bf36951f
Aradmehdi <130738810+Aradmehdi@users.noreply.github.com>
2025-10-18 18:20:28 +0200

    Update vmss.sh

--- docs/code/vmss.sh
+++ docs/code/vmss.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# Create VM Scale Set with Load Balancer
+az vmss create \
+  -g rg-az104-app \
+  -n vmss-app \
+  --image Ubuntu2204 \
+  --upgrade-policy-mode automatic \
+  --admin-username azureuser \
+  --generate-ssh-keys \
+  --vnet-name vnet-app \
+  --subnet app \
+  --lb lb-app
 
